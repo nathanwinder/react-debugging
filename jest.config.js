c034f6318b131e4df06d18311ab7784d312071c8
Nathan Winder <winder.nathan@gmail.com>
2018-10-29 11:14:40 -0500

    fixed build issue

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/example/"],
+  testPathIgnorePatterns: ["/example/", "lib"],
   coveragePathIgnorePatterns: ["/example/", "jsdom-setup.ts"],
   setupFiles: ["./src/jsdom-setup.ts"],
   snapshotSerializers: ["enzyme-to-json/serializer"]
